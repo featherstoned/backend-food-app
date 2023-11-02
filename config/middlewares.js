ea0af1e98c02539f6af0a04b525ce5a3e0ab0331
Xochitl Mora <124631246+featherstoned@users.noreply.github.com>
2023-11-02 10:25:44 -1000

    updating to to fix middlewares errors

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,20 +1,30 @@
-module.exports = [
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::logger',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
-  {
-    settings: {
-      cors: {
-        enabled: true,
-        origin: ['https://frontend-food-app-six.vercel.app'],
-      },
+// module.exports = [
+//   'strapi::errors',
+//   'strapi::security',
+//   'strapi::cors',
+//   'strapi::poweredBy',
+//   'strapi::logger',
+//   'strapi::query',
+//   'strapi::body',
+//   'strapi::session',
+//   'strapi::favicon',
+//   'strapi::public',
+//   {
+//     settings: {
+//       cors: {
+//         enabled: true,
+//         origin: ['https://frontend-food-app-six.vercel.app'],
+//       },
+//     },
+//   },
+// ];
+
+//& updating to fix middlewares errors
+module.exports = {
+  settings: {
+    cors: {
+      enabled: true,
+      origin: ['https://frontend-food-app-six.vercel.app'],
     },
   },
-];
+};
