6a83170e8de702290ff5f66823ee7ddda49fcefd
Xochitl Mora <124631246+featherstoned@users.noreply.github.com>
2023-11-02 10:55:12 -1000

    middleswares.js update

--- config/middlewares.js
+++ config/middlewares.js
@@ -20,11 +20,22 @@
 // ];
 
 //& updating to fix middlewares errors
-module.exports = {
-  settings: {
-    cors: {
+module.exports = [
+  'strapi::errors',
+  'strapi::security',
+  'strapi::cors',
+  'strapi::poweredBy',
+  'strapi::logger',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
+  {
+    name: 'strapi::cors',
+    config: {
       enabled: true,
       origin: ['https://frontend-food-app-six.vercel.app'],
     },
   },
-};
+];
